59e573138ee042337a6c172e9eea76d87fb22ef8
Le Ventures <dev@leventures.com>
2022-08-23 20:33:13 +0100

    remove melisearch

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,11 @@
 module.exports = () => ({
-    meilisearch: {
-      config: {
-        // Your meili host
-        // host: "http://localhost:7700",
-        host: "http://localhost:1337",
-        // Your master key or private key
-        apiKey: "masterKey",
-      }
-    }
+    // meilisearch: {
+    //   config: {
+    //     // Your meili host
+    //     // host: "http://localhost:7700",
+    //     host: "http://localhost:1337",
+    //     // Your master key or private key
+    //     apiKey: "masterKey",
+    //   }
+    // }
 })
